887d52c6bde8fec1908e0f4acb6c84278c8e35f8
billigoat76 <nvtnwr99@gmail.com>
2024-06-04 01:54:32 +0530

    Utilities added

---
+++ src/utils/api/index.js
@@ -0,0 +1,86 @@
+class API {
+    constructor() {
+      this.baseURL = 'https://api.spacexdata.com/v4'
+    }
+  
+    async get(url) {
+      try {
+        const response = await fetch(this.baseURL + url, {
+          headers: {
+            'content-type': 'application/json',
+          },
+        })
+        const data = await response.json()
+  
+        return Promise.resolve({
+          data,
+          status: response.status,
+        })
+      } catch (e) {
+        console.log(e)
+        return Promise.reject(e)
+      }
+    }
+  
+    async post(url, payload) {
+      try {
+        const response = await fetch(this.baseURL + url, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        })
+        const data = await response.json()
+  
+        return Promise.resolve({
+          data,
+          status: response.status,
+        })
+      } catch (e) {
+        console.log(e)
+        return Promise.reject(e)
+      }
+    }
+  }
+  
+  const request = new API()
+  
+  export async function getLaunches({ page = 1, limit = 10, query = {} }) {
+    return request.post(`/launches/query`, {
+      query,
+      options: {
+        page,
+        populate: [
+          {
+            path: 'payloads',
+  
+            select: {
+              name: 1,
+              orbit: 1,
+            },
+          },
+          {
+            path: 'rocket',
+            select: {
+              name: 1,
+              engines: 1,
+              country: 1,
+              company: 1,
+            },
+          },
+          {
+            path: 'launchpad',
+            select: {
+              locality: 1,
+              name: 1,
+            },
+          },
+        ],
+        limit,
+        sort: {
+          flight_number: 'asc',
+        },
+      },
+    })
+  }
---
+++ src/utils/helpers.js
@@ -0,0 +1,74 @@
+export const tableColumns = [
+  'No:',
+  'Launched (UTC)',
+  'Location',
+  'Mission',
+  'Orbit',
+  'Launched Status',
+  'Rocket',
+]
+
+export function getColumnWidth(type) {
+  const sizes = {
+    1: 32,
+    2: 180,
+    3: 100,
+    4: 120,
+    5: 50,
+    6: 98,
+    7: 92,
+  }
+
+  return sizes[type]
+}
+
+export function getLaunchStatus(success) {
+  let launchStatus = 'Upcoming'
+
+  if (success) {
+    launchStatus = 'Success'
+  } else if (success === false) {
+    launchStatus = 'Failed'
+  }
+
+  return launchStatus
+}
+
+export function getDate(dateStr) {
+  const date = new Date(dateStr)
+
+  const datePart = date
+    .toLocaleDateString(undefined, {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    })
+    .split(',')[1]
+
+  const timePart = date.toLocaleTimeString().slice(0, -3)
+
+  return `${datePart} at ${timePart}`
+}
+
+export function calculatePastDate(n, type) {
+  let d = new Date()
+
+  if (type === 'year') {
+    d.setFullYear(d.getFullYear() - n)
+  }
+
+  if (type === 'month') {
+    d.setMonth(d.getMonth() - n)
+  }
+
+  if (type === 'week') {
+    const totalMillseconds = n * 7 * 24 * 60 * 60 * 1000
+
+    d = d.getTime() - totalMillseconds
+  }
+
+  const revisedDated = new Date(d)
+
+  return revisedDated
+}
